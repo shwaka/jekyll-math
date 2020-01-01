029c2fbfc0006142b7d881848cd800aed7c67167
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-01-01 22:47:01 +0900

    bugfix concerning collections in jekyll

--- lib/jekyll-math/data-handler.rb
+++ lib/jekyll-math/data-handler.rb
@@ -1,13 +1,22 @@
 # coding: utf-8
+require 'pathname'
+
 module JekyllMath
   class DataHandler
     @@data_key = :crossref
     def self.from_context(context)
       context = context
       site = context.registers[:site]
-      current_page = site.pages.find{|page|
-        current_page_path = context.registers[:page]["path"]
-        page["path"] == current_page_path }
+      pages = site.collection_names.inject(site.pages){|_pages,col_name|
+        _pages + site.collections[col_name].docs
+      }
+      site_source_path = Pathname.new(site.source)
+      current_page_path = site_source_path / context.registers[:page]["path"]
+      current_page = pages.find{|page|
+        path = site_source_path / page.path
+        # ↑元々 page.path が絶対パスなら，path = page.path となる
+        current_page_path == path
+      }
       return self.new(current_page)
     end
 
